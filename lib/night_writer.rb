163e90db5c8924d7fa5f8434865820191f4a6fca
Candace Eckels <candace.eckels@gmail.com>
2022-04-26 15:49:09 -0600

    Prints braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,17 +1,14 @@
 #this is a runner file so there is no spec file for it
-
-require_relative 'english_to_braille'
-require_relative 'translator'
 require 'pry'
 
 input_file = File.open(ARGV[0], "r")
 message_reader = input_file.read.strip
 input_file.close
 
-translate = Translator.new(message_reader)
+chars = message_reader.length
 
 writer = File.open(ARGV[1], "w")
-writer.write(translate.split_lines)
+writer.write(message_reader)
 writer.close
 
-puts "Created '#{ARGV[1]}' containing #{translate.char_length} characters"
+puts "Created '#{ARGV[1]}' containing #{chars} characters"
